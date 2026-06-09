b4cd2ece1f1897d65153e06048ba9dab1aba68f3
cc <cc.com>
2026-06-10 00:05:25 +0800

    webpack

--- 1 webpack/src/index.js
+++ 1 webpack/src/index.js
@@ -1,5 +1,19 @@
 // 1 使用es6 导入语法 导入jquery
 import $ from 'jquery'
+// 导入样式（在webpack中，一切皆模块，都可以通过es6导入语法进行导入和使用） 
+import './css/index.css'
+import './css/index.less'
+
+// import a from './css/index.css'
+// import b from './css/index.less'
+// console.log(a,b)  // a,b为空对象 只需要加载样式文件 就可以了 
+// 如果某个模块使用from接收到的成员为undefined 则没有必要进行接收
+
+// 导入图片
+import logo from './images/logo.png'
+console.log(logo) // logo为图片的绝对路径或者base64编码
+// 将图片添加到页面中
+$('.box').attr('src', logo)
 
 // 2 使用jquery 定义入口函数
 $(function () {
@@ -7,3 +21,14 @@ $(function () {
     $('li:odd').css('backgroundColor', 'pink')
     $('li:even').css('backgroundColor', 'skyblue')
 })
+
+//测试高级语法
+// 定义装饰器
+function info(target) {
+    target.info = 'Person info'
+}
+
+@info
+class Person{}
+
+console.log(Person.info)
--- 1 webpack/webpack.config.js
+++ 1 webpack/webpack.config.js
@@ -1,6 +1,9 @@
 const path = require('path');
 const HtmlPlugin = require('html-webpack-plugin'); // 1 引入html-webpack-plugin插件
 
+const {CleanWebpackPlugin} = require('clean-webpack-plugin'); // 3 引入clean-webpack-plugin插件
+
+
 // 2 创建html-webpack-plugin插件实例 
 const htmlPlugin = new HtmlPlugin({
     template: './src/index.html', // 指定需要复制的文件路径
@@ -16,16 +19,58 @@ module.exports = {
 
     output: {
         path: path.join(__dirname, 'dist'),
-        filename: 'bundle.js'
+        // filename: 'bundle.js'
+        filename: 'js/bundle.js'
     },
     // 指定输出文件
 
-    plugins: [htmlPlugin],  // 3 通过plugin节点配置 使html-webpack-plugin插件生效
+    plugins: [htmlPlugin,new CleanWebpackPlugin()],  // 3 通过plugin节点配置 使html-webpack-plugin和clean-webpack-plugin插件生效 
 
     devServer:{
         open: true,
-        host: '123.0.0.1',
+        host: '127.0.0.1',
         port: 80,
-    }
+    },
     // 首次打包成功后 自动打开浏览器的地址和端口
+
+    module: {
+        rules: [
+            {
+                test: /\.css$/,    //表示匹配的文件类型 正则匹配.css文件
+                use: ['style-loader', 'css-loader'] // 指定使用的loader 顺序是固定的 是从右向左调用 处理后把结果转交给webpack
+            },
+            // 配置css文件的处理规则
+
+            {
+                test: /\.less$/,    //表示匹配的文件类型 正则匹配.less文件
+                use: ['style-loader', 'css-loader', 'less-loader'] // 配置less文件的处理规则
+            },
+            // 配置less文件的处理规则
+
+            {
+                test: /\.(png|jpg|gif|jpeg)$/,
+                // use: 'url-loader?limit=1000'  // limit 限制图片大小 1000字节内图片会转成base64编码 否则会转成图片路径
+                use: 'url-loader?limit=1000&outputPath=images/'  // outputPath指定图片输出的位置
+                // use: [
+                //     {
+                //         loader: 'url-loader',
+                //         options: {
+                //             limit: 5000,
+                //             outputPath: 'images/'
+                //         }
+                //     }
+                // ]
+            },
+            // 配置图片的处理规则
+
+            {
+                test: /\.js$/,
+                use: 'babel-loader',
+                exclude: /node_modules/  // 排除node_modules下的文件 因为第三方包中js文件兼容性已经处理了
+            },
+            // 处理高级语法 js文件的处理规则
+        ]
+            
+        
+    },
 }
